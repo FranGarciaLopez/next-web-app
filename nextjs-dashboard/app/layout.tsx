dde86c758db9991f63e53d78439b3710586e415a
FranGarciaLopez <frangalo34@gmail.com>
2023-12-06 00:00:21 +0000

    init

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -1,3 +1,6 @@
+import '@/app/ui/global.css';
+import { montserrat } from './ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${montserrat.className} antialiased`}>
+        {children}
+      </body>
+
+      <footer className='py-10 flex justify-center align-items-center'>
+        Hecho con Next JS y Tailwind CSS
+      </footer>
     </html>
   );
 }
