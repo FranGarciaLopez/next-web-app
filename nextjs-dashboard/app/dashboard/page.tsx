dccd611e6e3c516fc34738911de094cbdbfa6be1
Fran <frangalo34@gmail.com>
2024-01-08 12:51:32 +0100

    add skeleton foor RevenewChart & Card on dashboard page

--- nextjs-dashboard/app/dashboard/page.tsx
+++ nextjs-dashboard/app/dashboard/page.tsx
@@ -4,7 +4,7 @@ import RevenueChart from "../ui/dashboard/revenue-chart";
 import CardWrapper from "../ui/dashboard/cards";
 import { lusitana } from "../ui/fonts";
 import { Suspense } from "react";
-import { RevenueChartSkeleton } from "../ui/skeletons";
+import { CardsSkeleton, RevenueChartSkeleton } from "../ui/skeletons";
 
 
 export default async function Page() {
@@ -16,12 +16,14 @@ export default async function Page() {
                             Dashboard
                      </h1>
                      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-                            <CardWrapper />
+                            <Suspense fallback={<CardsSkeleton />}>
+                                   <CardWrapper />
+                            </Suspense>
                      </div>
                      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-                            <Suspense  fallback={<RevenueChartSkeleton />}>
-                            </Suspense>
+                            <Suspense fallback={<RevenueChartSkeleton />}>
                                    <RevenueChart />
+                            </Suspense>
                             <LatestInvoices latestInvoices={latestsInvoices} />
                      </div>
               </main>
