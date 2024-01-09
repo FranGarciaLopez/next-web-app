231bc7b8cbfe2c757f2d5ae359db69660f68ca22
Fran <frangalo34@gmail.com>
2024-01-09 17:51:25 +0100

    fetch customers in their page

--- nextjs-dashboard/app/dashboard/customers/page.tsx
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -1,9 +1,20 @@
 export default function CustomersPage() {
+export default async function CustomersPage({
+       searchParams,
+}: {
+       searchParams?: {
+              query?: string;
+       };
+}) {
+       const query = searchParams?.query || '';
+       
+       const customers = await fetchFilteredCustomers(query);
+
        return (
-              <main>
-                     <p>
-                            Customers page
-                     </p>
-              </main>
+              <div className="w-full">
+                     <Suspense key={query} fallback={<TableRowSkeleton />}>
+                            <Table customers={customers} />
+                     </Suspense>
+              </div>
        );
 }
